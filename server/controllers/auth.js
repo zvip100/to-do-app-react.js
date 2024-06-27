1d4f105592f62f3c4b35d3412e20ca185efedabb
Abraham Neuwirth <abeneuwirth@gmail.com>
2024-06-26 22:52:08 -0400

    formatting

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,19 +1,21 @@
-import db from './db.js';
-import boom from '@hapi/boom';
-import bcrypt from 'bcrypt';
-
+import db from "./db.js";
+import boom from "@hapi/boom";
+import bcrypt from "bcrypt";
 
 export const login = async (username, password) => {
-    const user = await db.oneOrNone('SELECT * FROM todos.person WHERE name = ${username}', {
-        username
-    });
+  const user = await db.oneOrNone(
+    "SELECT * FROM todos.person WHERE name = ${username}",
+    {
+      username,
+    }
+  );
 
-    if(!user) throw boom.forbidden('User does not exist');
-    let isPasswordCompared = await bcrypt.compare(password, user.pass);
+  if (!user) throw boom.forbidden("User does not exist");
+  let isPasswordCompared = await bcrypt.compare(password, user.pass);
 
-    // if(!isPasswordCompared){
-    //     throw boom.forbidden('Password does not match');
-    // }
+  // if(!isPasswordCompared){
+  //     throw boom.forbidden('Password does not match');
+  // }
 
-    return user;
-}
+  return user;
+};
--- server/controllers/db.js
+++ server/controllers/db.js
@@ -1,9 +1,6 @@
-
 import pgPromise from "pg-promise";
 
 const pgp = pgPromise();
 const db = pgp(process.env.DATABASE_URL);
-
-
 
 export default db;
